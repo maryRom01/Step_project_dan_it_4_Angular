0bf6dd7788cd3dd658d4c0b1c8c624664e084b29
maryRom01 <tester.automation.11@gmail.com>
2024-07-23 13:19:32 -0400

    change routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,12 +4,12 @@ import { CustomerComponent } from "./customer/customer.component";
 
 const routeConfig: Routes = [
     {
-        path: 'api/v1/customers',
+        path: 'customers',
         component: ListComponent,
         title: 'List of customers'
     },
     {
-        path: 'api/v1/customers/customer/:id',
+        path: 'customers/customer/:id',
         component: CustomerComponent,
         title: 'Customer'
     }
